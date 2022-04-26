be0386cfb2f63c191b468527e9f6fbd7ffebd87a
one <little.one@qq.com>
2022-04-26 18:18:10 +0800

    docs

---
+++ readme.md
@@ -0,0 +1,1 @@
+前端领域驱动整洁架构，[架构文档](https://www.yuque.com/xiaoyi-avok2/gg61bh/frpekd)
--- src/modules/auth/adts/hanlders.ts
+++ src/modules/auth/adts/hanlders.ts
@@ -3,14 +3,15 @@ import * as app from '../app'
 
 window.addEventListener('load', app.检测登录状态)
 
-window.addEventListener('auth', (e: any) => {
+window.addEventListener('auth', (e: CustomEvent) => {
   const { type, payload } = e.detail
 
   switch (type) {
     case 'login':
       const { username, password } = payload.target.elements
-      return app.登录(username.value, password.value)
+      app.登录(username.value, password.value)
+      break
     default:
-      return false
+      break
   }
 })
